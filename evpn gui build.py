f5c937bf1903cdff37a9fac8d47a83d4063047c1
AMILABS <jsicuran@amilabs.com>
2020-05-19 22:43:24 -0400

    Add files via upload

--- evpn gui build.py
+++ evpn gui build.py
@@ -4,11 +4,12 @@
 
 
 
-#####
+########
 #imports
 ########
 import PySimpleGUI as sg
 import netmiko
+import getpass
 
 ##### connect to lab evpn switch ####
 from netmiko import ConnectHandler
@@ -20,21 +21,41 @@ iosv_l2_s1 = {
     'password': 'admin',
 }
 
+
+#def evpn():
 net_connect = ConnectHandler(**iosv_l2_s1)
-output = net_connect.send_command('sh bgp l2vpn evpn ')
-########################################################
+output = net_connect.send_command('sh l2route evpn mac all ')
+
+#########################################################
 
 ############ LAUNCH GUI ################
 
+###################LOGIN TO SWITCH POPUP#################
+
+
+
+#########################################################
+
+#########MAIN APPLICATION################################
+
+
 sg.theme('Light Blue')
 
-layout = [  [sg.Text('Press MAC ROUTE:')],
-            [sg.Button('MAC ROUTES'), sg.Button('Clear'), sg.Button('Exit')],
-            [sg.Output(size=(60,50), key='-OUTPUT-')],
-            [sg.In(key='-IN-')]]
+layout = [  [sg.Text('BGP-EVPN Detail:')],
+            [sg.Button('BGP Summary'), sg.Button('BGP MAC Routes'), sg.Button('Layer2 MAC Routes'), sg.Button('BGP NLRI Type 2'), sg.Button('BGP Neighbors')],
+            [sg.Text('VXLAN NVE/VNI Detail:')],
+            [sg.Button('VXLAN'), sg.Button('VXLAN Interfaces'), sg.Button('VNI'), sg.Button('NVE Peers'), sg.Button('NVE Peer Detail')],
+            [sg.Text('ISIS Underlay Detail:')],
+            [sg.Button('ISIS Adjacency'), sg.Button('ISIS Routing'), sg.Button('ISIS Topology'), sg.Button('ISIS Database'), sg.Button('ISIS Database Detail')],
+
+            [sg.Button('Clear'), sg.Button('Exit')],
+
+            [sg.Text('Enter CLI Command'), sg.In(key='-IN-')],
+            [sg.Output(size=(70,40), key='-OUTPUT-')]]
+            #[sg.In(key='-IN-')]]
 
 
-window = sg.Window('Window Title', layout)
+window = sg.Window('BGP EVPN Monitor Utility', layout)
 
 while True:             # Event Loop
     event, values = window.read()
@@ -43,8 +64,38 @@ while True:             # Event Loop
         break
     if event == 'Clear':
         window['-OUTPUT-'].update('')
-    if event == 'MAC ROUTES':
-        window['-OUTPUT-'].update(output)
+    if event == 'BGP Summary':
+        window['-OUTPUT-'].update(net_connect.send_command('sh bgp l2vpn evpn summ '))
+    if event == 'BGP MAC Routes':
+        window['-OUTPUT-'].update(net_connect.send_command('sh bgp l2vpn evpn '))
+    if event == 'Layer2 MAC Routes':
+        window['-OUTPUT-'].update(net_connect.send_command('sh l2route mac all'))
+    if event == 'BGP NLRI Type 2':
+        window['-OUTPUT-'].update(net_connect.send_command('sh bgp l2vpn evpn route-type 2 '))
+    if event == 'BGP Neighbors':
+        window['-OUTPUT-'].update(net_connect.send_command('sh bgp l2vpn evpn neighbors '))
+    if event == 'VXLAN':
+        window['-OUTPUT-'].update(net_connect.send_command('sh vxlan '))
+    if event == 'VXLAN Interfaces':
+        window['-OUTPUT-'].update(net_connect.send_command('sh vxlan interface '))
+    if event == 'VNI':
+        window['-OUTPUT-'].update(net_connect.send_command('sh nve vni '))
+    if event == 'NVE Peers':
+        window['-OUTPUT-'].update(net_connect.send_command('sh nve peers '))
+    if event == 'NVE Peer Detail':
+        window['-OUTPUT-'].update(net_connect.send_command('sh nve peers detail '))
+    if event == 'ISIS Adjacency':
+        window['-OUTPUT-'].update(net_connect.send_command('sh isis adjacency '))
+    if event == 'ISIS Routing':
+        window['-OUTPUT-'].update(net_connect.send_command('sh isis route detail '))
+    if event == 'ISIS Topology':
+        window['-OUTPUT-'].update(net_connect.send_command('sh isis topology '))
+    if event == 'ISIS Database':
+        window['-OUTPUT-'].update(net_connect.send_command('sh isis database '))
+    if event == 'ISIS Database Detail':
+        window['-OUTPUT-'].update(net_connect.send_command('sh isis database detail '))
+
+
 window.close()
 
 ###############
